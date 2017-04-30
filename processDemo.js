df31f4a26039b34e7ce6e65c9ef1d3b7cf8f3284
John Laine <john.laine5896@gmail.com>
2017-04-29 17:26:18 -0700

    Add processDemo

---
+++ processDemo.js
@@ -0,0 +1,18 @@
+// console.log(process.argv);
+
+
+// process.argv.forEach((arg, i) => {
+//     console.log(`ARG-${i} : ${arg}`);
+// });
+
+
+//console.log(`Hello ${process.argv[2]}, how are you`);
+
+
+// setTimeout(() => {}, 2000);
+// process.on('exit', () => {
+//     console.log(`OK ${process.argv[2]}, let's get out of here!`);
+// });
+
+
+//console.log(process.env);
