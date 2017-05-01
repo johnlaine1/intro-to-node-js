7bc082e10119c88e00ba3cde0d2d337e34aadda0
John Laine <john.laine5896@gmail.com>
2017-05-01 13:45:43 -0700

    Update processDemo

--- processDemo.js
+++ processDemo.js
@@ -1,18 +1,28 @@
+console.log(process);
+
 // console.log(process.argv);
 
-
+/**
+ * Make argv array a little prettier.
+ */ 
 // process.argv.forEach((arg, i) => {
 //     console.log(`ARG-${i} : ${arg}`);
 // });
 
 
-//console.log(`Hello ${process.argv[2]}, how are you`);
+/**
+ * Use the first argument passed in when executing a file.
+ */ 
+// console.log(`Hello ${process.argv[2]}, how are you`);
 
 
+/**
+ * use the event emitter 'on' method.
+ */ 
+// console.log('Program is running');
 // setTimeout(() => {}, 2000);
 // process.on('exit', () => {
 //     console.log(`OK ${process.argv[2]}, let's get out of here!`);
 // });
 
 
-//console.log(process.env);
